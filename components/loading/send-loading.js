576fd613241d2cb81d8b4608edd569ebccfb0cf9
yosemite <yosemite@medzonedeMacBook-Pro.local>
2018-01-06 18:15:23 +0800

    完成菊花loading

--- components/loading/send-loading.js
+++ components/loading/send-loading.js
@@ -1,44 +1,9 @@
 // components/loading/send-loading.js
-const DEFAULT_SIZE = '15px'
+const DEFAULT_WIDTH = 2
+const DEFAULT_HEIGHT = 5
 const DEFAULT_ANIMATION_TIME = 1.2
 const DEFAULT_ITEM_COUNT = 8
-const DEFAULT_ITEM_WIDTH_PRECENT = 1.0 / 5.0
-
-class ItemFrame {
-  constructor(itemCount, index, itemWidth, size, sumAnimationTime) {
-    // 计算每次需要转动的角度
-    let offsetRad = (2.0 * Math.PI / 360) * (360 / itemCount) // 转换成弧度
-    // 计算所处坐标
-    this.itemX = size / 2.0 * Math.sin(offsetRad) - itemWidth * 0.5
-    this.itemY = size / 2.0 * Math.cos(offsetRad)
-    // 计算当前偏移角度
-    this.offsetAngle = 360 / itemCount * index
-    // 计算当前帧所处时间
-    this.showingTime = sumAnimationTime / itemCount * index
-  }
-
-  /**
-   * 计算item大小
-   * item的面积和默认占组件半径的1/5^2 * PI的大小
-   * @param size 组件大小
-   * @param itemCount 组件大小
-   * @param innerCricleRadius 空出的内圆半径
-   * @return list[0]:itemWidth
-   *         list[1]:itemHeight
-   */
-  getItemSize(size, itemCount, innerCricleRadius) {
-    let radius = size / 2.0
-    let itemHeight = size / 2.0 - innerCricleRadius
-    let itemWidth = size / 2.0 / DEFAULT_ITEM_WIDTH_PRECENT * Math.cos(_angle2Rad(360 / itemCount))
-    return [itemWidth, itemHeight]
-  }
-
-  getOffsetAngle
-
-  _angle2Rad(angle) {
-    return angle * (Math.PI / 180)
-  }
-}
+const DEFAULT_INNER_CRICLE_RADIUS = 2
 
 Component({
   /**
@@ -46,61 +11,88 @@ Component({
    */
   properties: {
     // 组件大小
-    size: {
-      type: String,
-      value: DEFAULT_SIZE
+    itemHeight: {
+      type: Number,
+      value: DEFAULT_HEIGHT
+    },
+    itemWidth: {
+      type: Number,
+      value: DEFAULT_WIDTH
     },
     // 菊花瓣颜色
     loadingItemColor: {
       type: String,
       value: 'grey' /*加载item的默认颜色为grey */
     },
-    // 组件边距
-    margin: {
-      type: String,
-      value: '10px'
-    },
     // 菊花花瓣数量
     itemCount: {
       type: Number,
-      value: 4
+      value: 8
     },
     // 中间空出圆的半径
-    innerCricleRadius: {
-      type: Number
+    innerCircleRadius: {
+      type: Number,
+      value: DEFAULT_INNER_CRICLE_RADIUS
     },
     // item转动一圈所需时长
     animationTime: {
-      type: Number
+      type: Number,
+      value: 1.2
     }
   },
   data: {
-    itemCount: 8 /*item个数*/,
-    itemWidth: 0 /*item宽度*/,
-    itemHeight: 0 /*item高度*/,
-    itemColor: "grey" /*item颜色*/,
-    margin: "2px" /*组件margin*/,
-    frameList: [] /*ItmeFrame list */
-  },
-  created: function () {
-    console.log("created")
+    itemStyleList: [] /*item样式list*/,
+    containerStyle: "" /*container样式*/
   },
   attached: function () {
-    console.log("attached")
+    let styleList = []
+    // 根据参数计算item样式
+    for (let index = 0; index < this.properties.itemCount; index++) {
+      // 样式模板
+      let styleTemplet = "top: {itemTop}px;left: {itemLeft}px;transform: rotate({itemAngle}deg);animation: line-spin-fade-loader {sumAnimationTime}s {itemShowingTime}s infinite ease-in-out;background-color: {itemColor};animation-fill-mode: both;position: absolute;width: {itemWidth}px;height: {itemHeight}px;"
+      // 计算当前item所应偏移角度
+      let curAngle = 360 / this.properties.itemCount * index
+      // 计算当前item对应正常坐标系中的弧度
+      let curRad = this._getRealRad(curAngle)
+      // 根据参数格式化样式
+      styleList.push(styleTemplet.format({
+        // 偏移要加上itemHeight 给左侧 上方腾出位置
+        // 相当于给坐标系平移itemHeight
+        itemTop: this._getRealXY((this.properties.innerCircleRadius + this.properties.itemHeight) * Math.sin(curRad)),
+        // -itemWidth * 0.5 是在计算item X轴上的偏移
+        itemLeft: this._getRealXY((this.properties.innerCircleRadius + this.properties.itemHeight) * Math.cos(curRad)),
+        itemAngle: curAngle,
+        sumAnimationTime: this.properties.animationTime,
+        itemShowingTime: this.properties.animationTime / this.properties.itemCount * index,
+        itemColor: "gray",
+        itemWidth: this.properties.itemWidth,
+        itemHeight: this.properties.itemHeight
+      }))
+    }
+    console.log(styleList)
+    // 组件的宽高 -- (item宽度 + 内圆半径) * 2
+    let containerWidth = (this.properties.itemHeight + this.properties.innerCircleRadius) * 2
+    // 更新界面
     this.setData({
-      itemCount: this.properties.itemCount
+      itemStyleList: styleList,
+      containerStyle: "width:{0}px;height:{0}px".format(containerWidth)
     })
-  },
-  ready: function () {
-    console.log("ready")
   },
   /**
    * 组件的方法列表
    */
   methods: {
-    getOffsetAngle: function (itemCount) {
-      return 360.0 / itemCount;
+    _angle2Rad: function (angle) {
+      return angle * (Math.PI / 180)
+    },
+    _getRealRad(angle) {
+      // 因为CSS3 角度为正时是顺时针旋转 0°在正常坐标系的270°上
+      // 所以要做转换才能获取到正确的弧度值
+      return this._angle2Rad(angle + 270)
+    },
+    _getRealXY: function (xOrY) {
+      return this.properties.itemHeight + this.properties.innerCircleRadius + xOrY
     }
-
   }
+
 })
